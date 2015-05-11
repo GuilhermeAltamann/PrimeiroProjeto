06739e502424079fbaa2b0ffa9cc5ac70d5070a9
Guilherme Pires Altmann <altmannguilherme@gmail.com>
2015-05-11 13:40:37 -0300

    commit

--- gen/br/com/treinaweb/primeiroprojeto/R.java
+++ gen/br/com/treinaweb/primeiroprojeto/R.java
@@ -15,23 +15,31 @@ public final class R {
     }
     public static final class id {
         public static final int buttonProximo=0x7f060002;
+        public static final int buttonVoltar=0x7f060007;
         public static final int editTextNome=0x7f060001;
+        public static final int textView1=0x7f060004;
+        public static final int textView2=0x7f060005;
+        public static final int textView3=0x7f060006;
         public static final int textViewMensagem=0x7f060000;
         public static final int textViewResposta=0x7f060003;
     }
     public static final class layout {
         public static final int activity_tela1=0x7f030000;
+        public static final int activity_tela2=0x7f030001;
     }
     public static final class string {
         public static final int aluno=0x7f040002;
         public static final int app_name=0x7f040000;
-        public static final int botao_ok=0x7f040007;
+        public static final int botao_ok=0x7f040008;
         public static final int botao_proximo=0x7f040006;
+        public static final int botao_voltar=0x7f040007;
         public static final int curso=0x7f040003;
+        public static final int ensino=0x7f04000a;
         public static final int msg=0x7f040004;
         public static final int ola_mundo=0x7f040001;
-        public static final int resposta=0x7f040008;
+        public static final int resposta=0x7f040009;
         public static final int texto_nome=0x7f040005;
+        public static final int titulo=0x7f04000b;
     }
     public static final class style {
         /** 
--- src/br/com/treinaweb/primeiroprojeto/Tela1.java
+++ src/br/com/treinaweb/primeiroprojeto/Tela1.java
@@ -1,24 +1,19 @@
 package br.com.treinaweb.primeiroprojeto;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 public class Tela1 extends Activity implements OnClickListener{
 
 	@Override
 	public void onClick(View v) {
-		// TODO Auto-generated method stub
-		TextView tvResultado = (TextView) findViewById(R.id.textViewResposta);
-		EditText txtNome = (EditText) findViewById(R.id.editTextNome);
-
-		String msg = "Nome: " + txtNome.getText().toString();
-		tvResultado.setText(msg);
-
+	  // TODO Auto-generated method stub
+	  Intent intencao = new Intent(Tela1.this, Tela2.class);
+	  startActivity(intencao);
 	}
 
 	@Override
