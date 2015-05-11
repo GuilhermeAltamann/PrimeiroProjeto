d98e8e91b6d8c25b00f625031b40e456d5d451bf
Guilherme Pires Altmann <altmannguilherme@gmail.com>
2015-05-11 13:22:08 -0300

    OK

--- src/br/com/treinaweb/primeiroprojeto/Tela1.java
+++ src/br/com/treinaweb/primeiroprojeto/Tela1.java
@@ -3,24 +3,32 @@ package br.com.treinaweb.primeiroprojeto;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class Tela1 extends Activity {
+public class Tela1 extends Activity implements OnClickListener{
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		TextView tvResultado = (TextView) findViewById(R.id.textViewResposta);
+		EditText txtNome = (EditText) findViewById(R.id.editTextNome);
+
+		String msg = "Nome: " + txtNome.getText().toString();
+		tvResultado.setText(msg);
+
+	}
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_tela1);
-	}
-	
-	public void cliqueBotaoOK(View v){
-		TextView tvResultado = (TextView) findViewById(R.id.textViewResposta);
-		EditText txtNome = (EditText) findViewById(R.id.editTextNome);
-		
-		String msg = "Nome: " + txtNome.getText().toString();
-		tvResultado.setText(msg);
-		
+
+		Button btn = (Button) findViewById(R.id.buttonProximo);
+		btn.setOnClickListener(this);
 	}
 
 }
